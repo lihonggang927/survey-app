2aa7b92ec6cd30aa2565d13ddd2bbdb268809860
lihonggang <lihg@sdas.org>
2019-12-18 20:32:45 +0800

    高校测评-前端程序

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- 展示模板 -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    用户名：<input />
+    密码：<input>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
